d77e48f327612160f77af960fc0256efa3ee2a24
Adhvaith Prasad <83104991+firescrypt@users.noreply.github.com>
2021-11-27 15:35:52 +0000

    changed to node git server

--- index.js
+++ index.js
@@ -13,15 +13,25 @@ const { getDatabase } = require('firebase-admin/database');
 // Get a database reference to our blog
 const db = getDatabase();
 var ref = db.ref('/');
-var pushover = require('pushover');
-var git = require("nodegit");
-var repos = pushover('/tmp/repos');
-const fs = require('fs');
-repos.on('push', function (push) {
-    // console.log('push ' + push.repo + '/' + push.commit
-    //     + ' (' + push.branch + ')'
-    // );
-    console.log(push);
+const path = require('path');
+const Server = require('node-git-server');
+ 
+const repos = new Server(path.resolve(__dirname, 'tmp'), {
+    autoCreate: true,
+    authenticate: ({type, repo, user}, next) => {
+      if(type == 'push') {
+        user((username, password) => {
+          console.log(username, password);
+          // next();
+        });
+      } else {
+        next();
+      }
+    }
+});
+const port = process.env.PORT || 7005;
+ 
+repos.on('push', (push) => {
     push.accept();
     var repo_clone_link = "https://tribble.adhvaithprasad.repl.co/"+push.repo ;
     var m = push.repo;
@@ -32,19 +42,54 @@ ref.child(repo_name).child(push.commit).set({
   branch:push.branch
 });
 
-
+    push.accept();
 });
-
-repos.on('fetch', function (fetch) {
-    console.log('fetch ' + fetch.commit);
+ 
+repos.on('fetch', (fetch) => {
+    console.log(`fetch ${fetch.commit}`);
     fetch.accept();
 });
-
-var http = require('http');
-var server = http.createServer(function (req, res) {
-
-    repos.handle(req, res);
-      
+ 
+repos.listen(port, () => {
+    console.log(`node-git-server running at http://localhost:${port}`)
 });
 
-server.listen(7000);
+
+
+
+
+
+
+
+
+// repos.on('push', function (push) {
+//     // console.log('push ' + push.repo + '/' + push.commit
+//     //     + ' (' + push.branch + ')'
+//     // );
+//     console.log(push);
+//     push.accept();
+//     var repo_clone_link = "https://tribble.adhvaithprasad.repl.co/"+push.repo ;
+//     var m = push.repo;
+// var repo_name = m.split('.')[0] ;
+// ref.child(repo_name).child(push.commit).set({
+//   commit: push.commit,
+//   repo:push.repo,
+//   branch:push.branch
+// });
+
+
+// });
+
+// repos.on('fetch', function (fetch) {
+//     console.log('fetch ' + fetch.commit);
+//     fetch.accept();
+// });
+
+// var http = require('http');
+// var server = http.createServer(function (req, res) {
+
+//     repos.handle(req, res);
+      
+// });
+
+// server.listen(7000);
